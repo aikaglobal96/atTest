4ba29a7b5dcc54237371ace6cb88bd80a5691425
Steeve-Handson <aikaglobal@Steeve-Handsons-MacBook-Pro.local>
2022-04-29 17:54:26 -0400

    push

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     
         
         <h1>je vous presente ma premiere push on github</h1>
         <p>salut je suis tres fier de moi aujourd'hui..
           je vais faire une bombe partout.</p>        
-      </header>
+      
     </div>
   );
 }
