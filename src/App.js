f3a3322012480a3f025f94ce176606487447d5eb
Steeve-Handson <aikaglobal@Steeve-Handsons-MacBook-Pro.local>
2022-04-29 17:40:57 -0400

    push

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          jái reussi enfin...
         </p>
         <a
           className="App-link"
