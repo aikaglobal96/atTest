28c6a2f3ac90b253455632c4a3aa08665b413316
Steeve-Handson <aikaglobal@Steeve-Handsons-MacBook-Pro.local>
2022-05-14 19:36:37 -0400

    update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ function App() {
      
         
         <h1>je vous presente ma premiere push on github</h1>
-        <p>salut je suis tres fier de moi aujourd'hui..
-          je vais faire une bombe partout.</p>        
+        <p>On a reussi avec le projet de communication
+          optrocom Tranning.</p>  
+          <p>il nous reste seulement a faire le site web..</p>
+          <p>ceci est un appercu de React Js déployé sur AWS
+            Amplifie par GitHub...</p>   
+            <p>on se voit vendredi prochain pour la reunion...</p> 
+            <p>passez une tres bonne semaine avec Dieu!</p>  
       
     </div>
   );
